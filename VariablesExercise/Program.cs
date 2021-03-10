72154d2504ba382d07952cd33096d2c7f12b6805
Jeremy Huddleston <jshudd@gmail.com>
2021-03-09 19:51:56 -0600

    initial commit

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myDogName = "Joe";
+
+            int dogAge = 3;
+
+            char dogInitial = 'J';
+
+            bool myBool = false;
+
+            double myDouble = 1.5;
+
+            decimal myDecimal = 2.7m;
+        }
+    }
+}
